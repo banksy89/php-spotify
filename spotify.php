65f7dc226fb2d00538409f6e43803ac1199698a8
Ashley Banks <ashleysmbanks89@gmail.com>
2013-12-05 16:10:26 +0000

    first commit

---
+++ spotify.php
@@ -0,0 +1,34 @@
+<?php
+
+class Spotify 
+{
+	static $base_url = 'http://ws.spotify.com';
+
+	public function tracks($string)
+	{
+		$result = self::fetch(self::build_url($string));
+		
+		return $result->tracks;
+	}
+
+	public function albums($string)
+	{
+		$result = self::fetch(self::build_url($string), 'album');
+		
+		return $result->albums;	
+	}
+
+	private function build_url($string, $type='track') 
+	{
+		$query = self::$base_url.'/search/1/'.$type.'.json?q='.urlencode($string);
+	}
+
+	private function fetch($query)
+	{
+		$result = file_get_contents($query);
+
+		return json_decode($result);
+	}
+}
+
+?>
